96cbc943e8ca98936766961ad29c80b1c1e4cee4
Jayasuryan1507 <jayasuryans360@gmail.com>
2025-09-04 12:34:49 +0530

    SpringBoot Scheduling

---
+++ Schedular-order-processing-service-main/src/main/java/com/codesnippet/order_processing_service/dto/OrderRequest.java
@@ -0,0 +1,13 @@
+package com.codesnippet.order_processing_service.dto;
+
+public class OrderRequest {
+    private String customerEmail;
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+}
